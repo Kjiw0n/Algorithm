daaeb198ee91b149bd173c9286a045fb099eb1b3
Kjiw0n <rwd0904@naver.com>
2025-08-08 22:13:13 +0900

    Implementation

---
+++ BOJ/[10871] X보다 작은 수 (2).js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(0, "utf8").trim().split("\n");
+
+const [N, X] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number);
+
+console.log(...arr.filter((x) => x < X));
